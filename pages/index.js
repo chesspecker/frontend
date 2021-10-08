9c97c67a0a9f32fc354a7a93679ce0dbab206efc
henribruvier <henri.bruvier@vizapp.fr>
2021-10-08 17:22:15 +0200

    orgenize code

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,9 @@
-import Head from 'next/head.js';
-import styles from '../styles/main.module.css';
+import LogginRegister from './loggin/Index';
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
-			<Head>
-				<title>chesspecker</title>
-				<meta name='description' content='chesspecker training app' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<div>
-				<img className={styles.imgLichess} src='./lichess.png' />
-				<h1 className={styles.title}>
-					Happy to see you
-					<br />
-					newcomer 👋
-				</h1>
-			</div>
-			<div className={styles.contentBottom}>
-				<a className={styles.btn} href='https://api.chesspecker.com/auth/login'>
-					SIGN IN WITH LICHESS
-				</a>
-			</div>
+		<div>
+			<LogginRegister />
 		</div>
 	);
 }
