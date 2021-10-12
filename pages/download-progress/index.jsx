4a3bd2a5afbcf724353a0623435b2c6c4a3cb713
henribruvier <henri.bruvier@vizapp.fr>
2021-10-12 10:17:02 +0200

    create progress bar

--- pages/download-progress/index.jsx
+++ pages/download-progress/index.jsx
@@ -2,12 +2,30 @@ import React from 'react';
 
 import PageHeader from '../../components/layouts/PageHeader.jsx';
 
+import ToggleSwitch from '../../components/layouts/ToggleSwitch.jsx';
 import style from './index.module.scss';
 
 function DownloadProgress(props) {
 	return (
 		<PageHeader>
-			<div className={style.container} />
+			<div className={style.container}>
+				<h2 className={style.title}>We are downloading your game 😀</h2>
+				<p className={style.description}>Status : 240/500 games </p>
+				<div className={style.progress_bar_container}>
+					<div className={style.progress_bar}>
+						<div
+							className={style.progress_bar_moving}
+							style={{transform: `translateX(10%)`}}
+						/>
+					</div>
+					<div
+						className={style.progress_bar_dot_container}
+						style={{transform: `translateX(10%) `}}
+					>
+						<div className={style.progress_bar_dot} />
+					</div>
+				</div>
+			</div>
 		</PageHeader>
 	);
 }
