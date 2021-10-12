6cf97b7af148c3a880357176e1796f9a6a303d9f
henribruvier <henri.bruvier@vizapp.fr>
2021-10-12 10:33:02 +0200

    add logic to slide bar

--- pages/download-progress/index.jsx
+++ pages/download-progress/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import http from '../../services/http-service.js';
 
 import PageHeader from '../../components/layouts/PageHeader.jsx';
 
@@ -6,6 +7,22 @@ import ToggleSwitch from '../../components/layouts/ToggleSwitch.jsx';
 import style from './index.module.scss';
 
 function DownloadProgress(props) {
+	const api = process.env.API;
+	const [percentage, setPercentage] = useState(0);
+
+	useEffect(() => {
+		const getPercentage = async () => {
+			const {data: percent} = await http.get(`${api}/worker`, {
+				withCredentials: true,
+			});
+			console.log(percent);
+		};
+
+		setInterval(() => {
+			getPercentage();
+			console.log('getPercent');
+		}, 5000);
+	}, []);
 	return (
 		<PageHeader>
 			<div className={style.container}>
@@ -15,12 +32,12 @@ function DownloadProgress(props) {
 					<div className={style.progress_bar}>
 						<div
 							className={style.progress_bar_moving}
-							style={{transform: `translateX(10%)`}}
+							style={{transform: `translateX(${percentage}%)`}}
 						/>
 					</div>
 					<div
 						className={style.progress_bar_dot_container}
-						style={{transform: `translateX(10%) `}}
+						style={{transform: `translateX(${percentage}%) `}}
 					>
 						<div className={style.progress_bar_dot} />
 					</div>
