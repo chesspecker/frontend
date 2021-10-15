956a6544a2039b451b8f22e7f2de43bf7f075124
henribruvier <henri.bruvier@vizapp.fr>
2021-10-15 12:10:01 +0200

    create analysing page and set redirection when download ends

--- pages/analysing/index.jsx
+++ pages/analysing/index.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {io} from 'socket.io-client';
-import Router from 'next/router.js';
+import Link from 'next/link.js';
 import http from '../../services/http-service.js';
 import PageHeader from '../../components/layouts/PageHeader.jsx';
 import ProgressBar from '../../components/02-Download-progress/ProgressBar.jsx';
@@ -8,7 +8,7 @@ import style from './index.module.scss';
 
 const ENDPOINT = 'https://api.chesspecker.com';
 
-function DownloadProgress() {
+function AnalyseProgress() {
 	const api = process.env.API;
 	const [percentage, setPercentage] = useState(0);
 	const [progress, setProgress] = useState(0);
@@ -44,16 +44,10 @@ function DownloadProgress() {
 		}, 500);
 	}, []);
 
-	useEffect(() => {
-		if (percentage === 1) {
-			Router.push('/analysing');
-		}
-	}, [percentage]);
-
 	return (
 		<PageHeader>
 			<div className={style.container}>
-				<h2 className={style.title}>We are downloading your game 😀</h2>
+				<h2 className={style.title}>Ok, now let's analyse your game !! 🔥</h2>
 				<p className={style.description}>
 					Status : {count}/{max} games{' '}
 				</p>
@@ -62,9 +56,12 @@ function DownloadProgress() {
 				<p className={style.noWorries}>
 					No worries, you can close this page and come back later
 				</p>
+				<Link href='/playing'>
+					<button>Next page</button>
+				</Link>
 			</div>
 		</PageHeader>
 	);
 }
 
-export default DownloadProgress;
+export default AnalyseProgress;
