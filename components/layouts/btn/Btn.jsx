191c22adf20cf5a4ce48cf731e9c0d16145bf3f2
henribruvier <henri.bruvier@vizapp.fr>
2021-10-18 18:26:55 +0200

    probleme solved

--- components/layouts/btn/Btn.jsx
+++ components/layouts/btn/Btn.jsx
@@ -3,7 +3,7 @@ import style from './Btn.module.scss';
 
 function Btn({link, onClick, children}) {
 	return (
-		<a className={style.btn} onClick={onClick(link)}>
+		<a className={style.btn} href={link} onClick={onClick}>
 			{children}
 		</a>
 	);
