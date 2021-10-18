5b04acc473af778c76858166f62da6b21aa50c4b
henribruvier <henri.bruvier@vizapp.fr>
2021-10-18 18:07:08 +0200

    last try

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,16 +1,8 @@
 import React, {useState, useEffect, useContext} from 'react';
-import useSets from '../../components/hooks/useSets.jsx';
 import PageHeader from '../../components/layouts/PageHeader.jsx';
 import GameMap from './GameMap.jsx';
 
 function Dashboard(props) {
-	const getSets = useSets();
-	const [gameSets, setGameSets] = useState([]);
-
-	useEffect(() => {
-		setGameSets(() => getSets);
-	});
-
 	return (
 		<PageHeader>
 			<GameMap />
