3aa32dd750759a4a3dd22ed928c1cbd73fd33cc7
Paul Nodet <paul.nodet@gmail.com>
2021-10-18 15:37:46 +0200

    fix undefined import

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useContext} from 'react';
 import PageHeader from '../../components/layouts/PageHeader.jsx';
 import LanguageContext from '../../components/layouts/LanguageContext.jsx';
-import GameMap from './gameMap.jsx';
+import GameMap from './GameMap.jsx';
 
 function Dashboard(props) {
 	const [language, setLanguage] = useState('en');
