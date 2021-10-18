2290e9b8afc617f367a24e1d0c2e66aca523ae98
henribruvier <henri.bruvier@vizapp.fr>
2021-10-18 10:31:25 +0200

    create useSets hooks

---
+++ components/hooks/useSets.jsx
@@ -0,0 +1,21 @@
+import {useState, useEffect} from 'react';
+import http from '../../services/http-service.js';
+
+export default function useSets() {
+	const api = process.env.API;
+	const [gameSet, setGameSet] = useState('');
+
+	useEffect(() => {
+		const getSets = async () => {
+			const {data: sets} = await http.get(`${api}/puzzles/sets`, {
+				withCredentials: true,
+			});
+			setGameSet(() => sets);
+			console.log(sets);
+		};
+
+		getSets();
+	}, []);
+
+	return gameSet;
+}
--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import useSets from '../../components/hooks/useSets.jsx';
 import PageHeader from '../../components/layouts/PageHeader.jsx';
 import GameSet from '../../components/layouts/sets/GameSet.jsx';
 import style from './index.module.scss';
 
 function Dashboard(props) {
+	const [gameSets, setGameSets] = useState(useSets());
+
 	return (
 		<PageHeader>
 			<div className={style.container}>
 				<div className={style.gameSet}>
-					<GameSet />
+					{gameSets && gameSets.map(s => <GameSet />)}
 				</div>
 			</div>
 		</PageHeader>
