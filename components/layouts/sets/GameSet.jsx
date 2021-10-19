b8c1e515fdde7125090edbf546d64d4b53a6c18e
pnxdxt <paul.nodet@gmail.com>
2021-10-19 12:37:02 +0000

    Fix code style issues with XO

--- components/layouts/sets/GameSet.jsx
+++ components/layouts/sets/GameSet.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import Btn from '../btn/Btn.jsx';
-import style from './GameSet.module.scss';
 import Stars from '../stars/Stars.jsx';
+import style from './GameSet.module.scss';
 
 function GameSet({sets, number, setCurrentSet}) {
 	return (
-		<>
-			<div className={style.set}>
-				<h3 className={style.title}>Set {number + 1}</h3>
-				<div className={style.list}>
-					<div className={style.list_element}>🏆: 24:37</div>
-					<div className={style.list_element}>⛔ : 18</div>
-				</div>
-				<Stars />
-				<Btn onClick={setCurrentSet}>Start</Btn>
+		<div className={style.set}>
+			<h3 className={style.title}>Set {number + 1}</h3>
+			<div className={style.list}>
+				<div className={style.list_element}>🏆: 24:37</div>
+				<div className={style.list_element}>⛔ : 18</div>
 			</div>
-		</>
+			<Stars />
+			<Btn onClick={setCurrentSet}>Start</Btn>
+		</div>
 	);
 }
 
