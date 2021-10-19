73c7e52837f237808d118fdfbe07de0304f8a675
henribruvier <henri.bruvier@vizapp.fr>
2021-10-19 15:07:41 +0200

    creat game categorie service

---
+++ services/gameCategorieService.js
@@ -0,0 +1,34 @@
+export const puzzleThemes = [
+	{
+		title: 'Opening',
+		description: 'A tactic during the first phase of the game',
+		category: 'Phases',
+	},
+	{
+		title: 'Middlegame',
+		description: 'A tactic during the midle phase of the game',
+		category: 'Phases',
+	},
+	{
+		title: 'Endgame',
+		description: 'A tactic during the last phase of the game',
+		category: 'Phases',
+	},
+	{
+		title: 'Rook endgame',
+		description: 'A endgame with only rooks and pawns',
+		category: 'Phases',
+	},
+];
+
+export const themesCategory = [
+	{nom: 'Recommended', id: '1'},
+	{nom: 'Phases', id: '1'},
+	{nom: 'Motifs', id: '1'},
+	{nom: 'Advanced', id: '1'},
+	{nom: 'Mates', id: '1'},
+	{nom: 'Special moves', id: '1'},
+	{nom: 'Goals', id: '1'},
+	{nom: 'Lengths', id: '1'},
+	{nom: 'Origin', id: '1'},
+];
