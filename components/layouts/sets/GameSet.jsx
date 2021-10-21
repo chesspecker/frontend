c297b2ecf769acd007fb525f07198a4e89d67713
henribruvier <henri.bruvier@vizapp.fr>
2021-10-21 17:06:48 +0200

    add supress icon

--- components/layouts/sets/GameSet.jsx
+++ components/layouts/sets/GameSet.jsx
@@ -1,12 +1,17 @@
 import Btn from '../btn/Btn.jsx';
 import Stars from '../stars/Stars.jsx';
 import style from './GameSet.module.scss';
+import Image from 'next/image.js';
+import supress from '../../../public/images/supress.svg';
 
 function GameSet({sets, _number, setCurrentSet}) {
 	return (
 		<div className={style.set}>
 			<h3 className={style.title}>{sets.title}</h3>
 			<div className={style.list}>
+				<div className={style.supress}>
+					<Image src={supress} />
+				</div>
 				<div className={style.list_element}>🏆: {sets.bestTime}</div>
 				<div className={style.list_element}>⛔ : {sets.tries}</div>
 			</div>
