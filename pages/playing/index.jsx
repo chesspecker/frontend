3b31c9468e1d2bbb88d0ffd75b8a8ff411438632
henribruvier <henri.bruvier@vizapp.fr>
2021-10-21 16:16:10 +0200

    console log in onMove

--- pages/playing/index.jsx
+++ pages/playing/index.jsx
@@ -108,6 +108,12 @@ function Index() {
 	const onMove = async (from, to) => {
 		const move = chess.move({from, to, promotion: 'x'});
 		const moves = chess.moves({verbose: true});
+		console.log(
+			'validation',
+			`${move.from}${move.to}`,
+			' = ',
+			history[moveNumber],
+		);
 
 		/**
 		 * FIXME: doesn't work, setPendingMove is undefined
