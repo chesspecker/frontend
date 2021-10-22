2a03258c610e31942d097f2a0f9268e09acb9a81
henribruvier <henri.bruvier@vizapp.fr>
2021-10-22 18:25:38 +0200

    update errorPopup

--- components/layouts/popup/ErrorPopup.jsx
+++ components/layouts/popup/ErrorPopup.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Image from 'next/image.js';
 import BtnSecondary from '../btn/BtnSecondary.jsx';
+import error from '../../../public/images/supress.svg';
 import BackgroundPopup from './BackgroundPopup.jsx';
 import ContainerPopup from './ContainerPopup.jsx';
 import style from './ErrorPopup.module.scss';
@@ -8,8 +10,9 @@ function ErrorPopup({onClick}) {
 	return (
 		<BackgroundPopup>
 			<ContainerPopup>
+				<Image src={error} width={70} height={70} />
 				<h3 className={style.title}>
-					Sorry your set must contain between 20 and 40 puzzle
+					Sorry your set must contain between 20 and 40 puzzles
 				</h3>
 				<BtnSecondary onClick={onClick}> Retry </BtnSecondary>
 			</ContainerPopup>
