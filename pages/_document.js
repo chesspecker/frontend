e42398e69aafac317691e583de300d69aa8921e6
Paul Nodet <paul.nodet@gmail.com>
2021-10-26 17:41:27 +0200

    Add favicon. Fix #34

--- pages/_document.js
+++ pages/_document.js
@@ -14,12 +14,31 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<link rel='icon' href='/favicon.ico' />
+					{/* Meta title */}
 					<meta property='og:title' content='ChessPecker' />
 					<meta
 						property='og:description'
-						content='An application to practice playing chess with the famous woodpecker method!'
+						content='An application to practice playing chess with the woodpecker method!'
 					/>
+					{/* Favicon */}
+					<link
+						rel='apple-touch-icon'
+						sizes='180x180'
+						href='public/favicon/apple-touch-icon.png'
+					></link>
+					<link
+						rel='icon'
+						type='image/png'
+						sizes='32x32'
+						href='public/favicon/favicon-32x32.png'
+					></link>
+					<link
+						rel='icon'
+						type='image/png'
+						sizes='16x16'
+						href='public/favicon/favicon-16x16.png'
+					></link>
+					<link rel='manifest' href='public/favicon/site.webmanifest'></link>
 				</Head>
 				<body>
 					<Main />
