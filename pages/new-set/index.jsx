f288646fb9f0c74238dadd9782780d78dc506e89
Paul Nodet <paul.nodet@gmail.com>
2021-10-26 19:06:46 +0200

    create new popup

--- pages/new-set/index.jsx
+++ pages/new-set/index.jsx
@@ -9,22 +9,27 @@ import {
 import Btn from '../../components/layouts/btn/Btn.jsx';
 import {useNewSetContext} from '../../components/context/NewSetContext.jsx';
 import ErrorPopupNewSet from '../../components/layouts/popup/ErrorPopupNewSet.jsx';
+import ErrorPopupNumerousChoices from '../../components/layouts/popup/ErrorPopupNumerousChoices.jsx';
 import style from './index.module.scss';
 
 function NewSet() {
 	const [choicesSelected, setChoicesSelected] = useState([]);
 	const {newSet, updateNewSetOptions} = useNewSetContext();
-	const [toggleErrorPopup, setToggleErrorPopup] = useState(false);
+	const [toggleHealthyMixPopup, setToggleHealthyMixPopup] = useState(false);
+	const [toggleNumerousChoicesPopup, setToggleNumerousChoicesPopup] = useState(false);
 
 	const handleClick = id => {
-		const bool =
-			(choicesSelected.length > 0 &&
-				id === 'healthyMix' &&
-				!choicesSelected.includes(id)) ||
-			(choicesSelected[0] === 'healthyMix' && id !== 'healthyMix');
+		const selectingHealthyMixWithOther =
+			choicesSelected.length > 0 &&
+			id === 'healthyMix' &&
+			!choicesSelected.includes(id);
+		const healthyMixAlreadySelected =
+			choicesSelected[0] === 'healthyMix' && id !== 'healthyMix';
+		const healthyMixError =
+			selectingHealthyMixWithOther || healthyMixAlreadySelected;
 
-		if (bool) {
-			setToggleErrorPopup(() => true);
+		if (healthyMixError) {
+			setToggleHealthyMixPopup(() => true);
 			return;
 		}
 
@@ -38,8 +43,8 @@ function NewSet() {
 			return;
 		}
 
-		if (choicesSelected.length > 3) {
-			setToggleErrorPopup(() => true);
+		if (choicesSelected.length > 2) {
+			setToggleNumerousChoicesPopup(() => true);
 			return;
 		}
 
@@ -56,8 +61,15 @@ function NewSet() {
 
 	return (
 		<PageHeader>
-			{toggleErrorPopup && (
-				<ErrorPopupNewSet onClick={() => setToggleErrorPopup(() => false)} />
+			{toggleHealthyMixPopup && (
+				<ErrorPopupNewSet
+					onClick={() => setToggleHealthyMixPopup(() => false)}
+				/>
+			)}
+			{toggleNumerousChoicesPopup && (
+				<ErrorPopupNumerousChoices
+					onClick={() => setToggleNumerousChoicesPopup(() => false)}
+				/>
 			)}
 			<div className={style.container}>
 				<h2 className={style.title}>
