5c7a72d5d3fce22f3d79791da54da65d70f2649e
henribruvier <henri.bruvier@vizapp.fr>
2021-10-26 17:44:58 +0200

    fix favicon link

--- pages/_document.js
+++ pages/_document.js
@@ -24,19 +24,19 @@ class MyDocument extends Document {
 					<link
 						rel='apple-touch-icon'
 						sizes='180x180'
-						href='public/favicon/apple-touch-icon.png'
+						href='../public/favicon/apple-touch-icon.png'
 					></link>
 					<link
 						rel='icon'
 						type='image/png'
 						sizes='32x32'
-						href='public/favicon/favicon-32x32.png'
+						href='../public/favicon/favicon-32x32.png'
 					></link>
 					<link
 						rel='icon'
 						type='image/png'
 						sizes='16x16'
-						href='public/favicon/favicon-16x16.png'
+						href='../public/favicon/favicon-16x16.png'
 					></link>
 					<link rel='manifest' href='public/favicon/site.webmanifest'></link>
 				</Head>
