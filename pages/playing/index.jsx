2017a6de643c3fd0591c8e8b23bce6330193b486
Paul Nodet <paul.nodet@gmail.com>
2021-10-30 10:37:33 +0200

    Handle multiple checkmates. Closes #46

--- pages/playing/index.jsx
+++ pages/playing/index.jsx
@@ -195,7 +195,7 @@ function Index() {
 		soundMove();
 
 		const isCorrectMove = validateMove(move);
-		if (isCorrectMove) {
+		if (isCorrectMove || chess.in_checkmate()) {
 			setFen(() => chess.fen());
 			setMoveNumber(previousMove => previousMove + 1);
 			checkPuzzleComplete(moveNumber);
@@ -226,7 +226,7 @@ function Index() {
 		const isCorrectMove = piece === history[moveNumber].slice(-1);
 		chess.move({from, to, promotion: piece});
 
-		if (isCorrectMove) {
+		if (isCorrectMove || chess.in_checkmate()) {
 			setFen(() => chess.fen());
 			setMoveNumber(previousMove => previousMove + 1);
 			checkPuzzleComplete(moveNumber);
