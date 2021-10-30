f4c21b6be11c368e090508f3e12a576de4e9638a
Paul Nodet <paul.nodet@gmail.com>
2021-10-30 19:25:00 +0200

    Work on #53

--- pages/playing/index.jsx
+++ pages/playing/index.jsx
@@ -119,8 +119,8 @@ function Index() {
 			title: 'Your turn',
 			subtitle: `Find the best move for ${orientation}.`,
 		};
-		setText(text);
-	}, [orientation]);
+		setText(() => text);
+	}, [orientation, actualPuzzle]);
 
 	/**
 	 * Retrieve current puzzle.
@@ -232,6 +232,10 @@ function Index() {
 			setMistakesNumber(previous => previous + 1);
 			setWrongMoveVisible(() => true);
 			setTimeout(() => setWrongMoveVisible(() => false), 300);
+			setText(() => ({
+				title: `That's not the move!`,
+				subtitle: `Try something else.`,
+			}));
 		}
 	};
 
