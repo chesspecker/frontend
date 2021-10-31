f3bd86105726db3e217b2231dc044b4ff2ff85dc
henribruvier <henri.bruvier@vizapp.fr>
2021-10-31 17:43:57 +0100

    dix wrong move not responsibve on mobile

--- pages/playing/index.jsx
+++ pages/playing/index.jsx
@@ -478,6 +478,7 @@ function Index() {
 					/>
 				)}
 				{startPopupVisible && <StartingPopup onStart={handleStart} />}
+				{wrongMoveVisible && <div className={style.wrong_move}>+3&quot;!</div>}
 
 				<div className={style.container}>
 					<div className={style.information_container}>
@@ -490,9 +491,6 @@ function Index() {
 					</div>
 					<div>
 						<div className={style.chessGroundContainer}>
-							{wrongMoveVisible && (
-								<div className={style.wrong_move}>+3&quot;!</div>
-							)}
 							<div className={style.chessGround_left_container} />
 							<ChessGround
 								fen={fen}
