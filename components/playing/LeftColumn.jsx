3ac7c0905cc9b2e6621a8ac38d43361392a66b4e
pnxdxt <paul.nodet@gmail.com>
2021-11-06 10:03:47 +0100

    Add better tooltips

--- components/playing/LeftColumn.jsx
+++ components/playing/LeftColumn.jsx
@@ -4,6 +4,8 @@ import Image from 'next/image.js';
 import rotate from '../../public/images/rotate.svg';
 import fastForward from '../../public/images/fast-forward.svg';
 import style from './LeftColumn.module.scss';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 
 export default function LeftColumn({
 	changeSoundStatus,
@@ -15,27 +17,35 @@ export default function LeftColumn({
 	return (
 		<div className={style.container}>
 			<div className={style.content}>
-				<a className={style.icon_mute} onClick={changeSoundStatus}>
-					{soundStatus ? <BsFillVolumeMuteFill /> : <BsFillVolumeUpFill />}
-				</a>
+				<Tippy content='Sound preference'>
+					<a className={style.icon_mute} onClick={changeSoundStatus}>
+						{soundStatus ? <BsFillVolumeMuteFill /> : <BsFillVolumeUpFill />}
+					</a>
+				</Tippy>
 			</div>
 			<div className={style.content}>
-				<a className={style.icon_rotate} onClick={switchOrientation}>
-					<Image src={rotate} width={50} height={50} />
-				</a>
+				<Tippy content='Flip board'>
+					<a className={style.icon_rotate} onClick={switchOrientation}>
+						<Image src={rotate} width={50} height={50} />
+					</a>
+				</Tippy>
 			</div>
 			{autoMove && (
 				<div className={`${style.content} ${style.autoMove_enabled}`}>
-					<a className={style.icon_enabled} onClick={toggleAutoMove}>
-						<Image src={fastForward} width={50} height={50} />
-					</a>
+					<Tippy content='Jump to next puzzle immediately'>
+						<a className={style.icon_enabled} onClick={toggleAutoMove}>
+							<Image src={fastForward} width={50} height={50} />
+						</a>
+					</Tippy>
 				</div>
 			)}
 			{!autoMove && (
 				<div className={`${style.content} ${style.autoMove_disabled}`}>
-					<a className={style.icon_disabled} onClick={toggleAutoMove}>
-						<Image src={fastForward} width={50} height={50} />
-					</a>
+					<Tippy content='Jump to next puzzle immediately'>
+						<a className={style.icon_disabled} onClick={toggleAutoMove}>
+							<Image src={fastForward} width={50} height={50} />
+						</a>
+					</Tippy>
 				</div>
 			)}
 		</div>
